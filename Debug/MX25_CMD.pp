68383e51ed5554690a92918241f2f6e0f77eaa55
yi-cheng Liao <yich_liao@davicom.com.tw>
2016-03-28 14:59:45 +0800

    MSP430F5529 MX25R1035F SPI flash memory init.

---
+++ Debug/MX25_CMD.pp
@@ -0,0 +1,19 @@
+# FIXED
+
+MX25_CMD.obj: ../MX25_CMD.c
+MX25_CMD.obj: ../MX25_CMD.h
+MX25_CMD.obj: ../MX25_DEF.h
+MX25_CMD.obj: c:/ti/ccsv6/ccs_base/msp430/include/msp430.h
+MX25_CMD.obj: c:/ti/ccsv6/ccs_base/msp430/include/msp430f5529.h
+MX25_CMD.obj: c:/ti/ccsv6/ccs_base/msp430/include/in430.h
+MX25_CMD.obj: c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.1/include/intrinsics.h
+MX25_CMD.obj: c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.1/include/intrinsics_legacy_undefs.h
+
+../MX25_CMD.c: 
+../MX25_CMD.h: 
+../MX25_DEF.h: 
+c:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+c:/ti/ccsv6/ccs_base/msp430/include/msp430f5529.h: 
+c:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.1/include/intrinsics.h: 
+c:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.1/include/intrinsics_legacy_undefs.h: 
---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnk_msp430f5529.cmd 
+
+C_SRCS += \
+../MX25_APP.c \
+../MX25_CMD.c 
+
+OBJS += \
+./MX25_APP.obj \
+./MX25_CMD.obj 
+
+C_DEPS += \
+./MX25_APP.pp \
+./MX25_CMD.pp 
+
+C_DEPS__QUOTED += \
+"MX25_APP.pp" \
+"MX25_CMD.pp" 
+
+OBJS__QUOTED += \
+"MX25_APP.obj" \
+"MX25_CMD.obj" 
+
+C_SRCS__QUOTED += \
+"../MX25_APP.c" \
+"../MX25_CMD.c" 
+
+
